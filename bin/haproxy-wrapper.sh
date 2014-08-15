2bbc958f5da408ce84944cf931188ad10eed27c9
sukrit007 <sukrit007@gmail.com>
2014-08-15 13:50:10 -0700

    Adding rsyslog

--- bin/haproxy-wrapper.sh
+++ bin/haproxy-wrapper.sh
@@ -7,7 +7,7 @@ PID_FILE="/var/run/haproxy.pid"
 trap "/etc/init.d/haproxy stop; exit" SIGINT SIGTERM
 
 #Check if there exists at-least 1 process running as haproxy user.
-while ps -u haproxy ;
+while ps -u haproxy 1>/dev/null ;
 do
   #Possible report process to etcd with ttl.
   sleep 20s
